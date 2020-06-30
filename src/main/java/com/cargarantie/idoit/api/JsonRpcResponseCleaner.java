f2c2af29d23ccfdf4c2645e26af1c61599345ab3
Mischke Sebastian <Sebastian.Mischke@cargarantie.com>
2020-06-30 07:54:49 +0200

    ITAUF-1104 - change method to stream

--- src/main/java/com/cargarantie/idoit/api/JsonRpcResponseCleaner.java
+++ src/main/java/com/cargarantie/idoit/api/JsonRpcResponseCleaner.java
@@ -4,9 +4,12 @@ import com.cargarantie.idoit.api.config.IdoitObjectMapper;
 import com.cargarantie.idoit.api.jsonrpc.CategoryRead;
 import com.cargarantie.idoit.api.jsonrpc.IdoitRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class JsonRpcResponseCleaner {
 
@@ -22,18 +25,6 @@ class JsonRpcResponseCleaner {
     return mapper.convertValue(result, request.getResponseClass());
   }
 
-  private Object mapToSingleCategory(List<Object> categories) {
-    switch (categories.size()) {
-      case 0:
-        return null;
-      case 1:
-        return removeIdentifierFields(categories.get(0));
-      default:
-        throw new IllegalArgumentException("Cannot convert a multi-category response"
-            + " to a single category result");
-    }
-  }
-
   /**
    * Custom category fields have a special format:
    * <pre>
@@ -45,22 +36,26 @@ class JsonRpcResponseCleaner {
    * causes the creation of empty parameter objects, instead of null values. We want null values, so
    * we clean identifier-only parameters
    */
-  private Object removeIdentifierFields(Object json) {
-    Map<String, Object> categoryJson = (Map<String, Object>) json;
+  private Map<String, Object> removeIdentifierFields(Map<String, Object> json) {
+    Set<Entry<String, Object>> entries = json.entrySet();
 
-    new HashMap<>(categoryJson).forEach((key, value) -> {
-      if (value instanceof Map) {
-        Map<String, Object> valueJson = (Map<String, Object>) value;
-        String identifier = (String) valueJson.remove("identifier");
+    return Stream.concat(
+        entries.stream().filter(e -> !(e.getValue() instanceof Map)),
+        entries.stream().filter(e -> e.getValue() instanceof Map)
+            .peek(e -> ((Map<?, ?>) e.getValue()).remove("identifier"))
+            .filter(e -> !((Map<?, ?>) e.getValue()).isEmpty())
+    ).collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+  }
 
-        if (identifier == null) {
-          return;
-        } else if (valueJson.isEmpty()) {
-          categoryJson.remove(key);
-        }
-      }
-    });
-
-    return categoryJson;
+  private Object mapToSingleCategory(List<Object> categories) {
+    switch (categories.size()) {
+      case 0:
+        return null;
+      case 1:
+        return removeIdentifierFields((Map<String, Object>) categories.get(0));
+      default:
+        throw new IllegalArgumentException("Cannot convert a multi-category response"
+            + " to a single category result");
+    }
   }
 }
